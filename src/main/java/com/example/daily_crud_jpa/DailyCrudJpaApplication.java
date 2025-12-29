de49a25c59e3acaa73f518f8816e28608f537476
master <ted3131@diquest.com>
2025-12-29 10:54:38 +0900

    Initial commit

---
+++ src/main/java/com/example/daily_crud_jpa/DailyCrudJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.daily_crud_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCrudJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyCrudJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/daily_crud_jpa/DailyCrudJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daily_crud_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyCrudJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
